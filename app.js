f4cc0cef65f191757519594c7cf66a55ba3e5adc
George Paloukis <gpalouk@codigia.com>
2012-04-16 19:41:56 +0300

    Created second screen (Blog, an almost full RSS Reader!!)

--- app.js
+++ app.js
@@ -24,18 +24,44 @@ Ext.application({
 
             items: [
                 {
-                    title: 'Home',
-                    iconCls: 'home',
-                    cls: 'home',
-                    scrollable: true,
+                    xtype: 'nestedlist',
+                    title: 'Sencha Blog',
+                    iconCls: 'star',
+                    displayField: 'title',
                     
-                    html: [
-                        '<img src="http://staging.sencha.com/img/sencha.png" />',
-                        '<h1>Welcome to Sencha Touch</h1>',
-                        "<p>You're creating the Getting Started app. This demonstrates how ",
-                        "to use tabs, lists and forms to create a simple app</p>",
-                        '<h2>Sencha Touch (2.0.0)</h2>'
-                    ].join("")
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    },
+
+                    store: {
+                        type: 'tree',
+
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    }
                 }
             ]
         });
