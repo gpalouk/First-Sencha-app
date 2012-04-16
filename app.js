c0c51679fb581614024c08953cb7abd0fdaac9d0
George Paloukis <gpalouk@codigia.com>
2012-04-16 20:18:27 +0300

    Final Getting Started app (3 screens)

--- app.js
+++ app.js
@@ -18,15 +18,75 @@ Ext.application({
     tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
 
     launch: function() {
-        Ext.create("Ext.tab.Panel", {
+        //The whole app UI lives in this tab panel
+        Ext.Viewport.add({
+            xtype: 'tabpanel',
             fullscreen: true,
             tabBarPosition: 'bottom',
 
             items: [
+                // This is the home page, just some simple html
                 {
-                    title: 'Contact',
-                    iconCls: 'user',
+                    title: 'Home',
+                    iconCls: 'home',
+                    cls: 'home',
+                    scrollable: true,
+                    html: [
+                        '<img height=260 src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>Building the Getting Started app</p>",
+                        '<h2>Sencha Touch (2.0.0)</h2>'
+                    ].join("")
+                },
+
+                // This is the recent blogs page. It uses a tree store to load its data from blog.json
+                {
+                    xtype: 'nestedlist',
+                    title: 'Blog',
+                    iconCls: 'star',
+                    cls: 'blog',
+                    displayField: 'title',
+
+                    store: {
+                        type: 'tree',
+
+                        fields: ['title', 'link', 'author', 'contentSnippet', 'content', {
+                            name: 'leaf',
+                            defaultValue: true
+                        }],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                },
+
+                // This is the contact page, which features a form and a button. The button submits the form
+                {
                     xtype: 'formpanel',
+                    title: 'Contact Us',
+                    iconCls: 'user',
                     url: 'contact.php',
                     layout: 'vbox',
 
@@ -34,19 +94,24 @@ Ext.application({
                         {
                             xtype: 'fieldset',
                             title: 'Contact Us',
-                            instructions: '(email address is optional)',
+                            instructions: 'Email address is optional',
+
                             items: [
                                 {
                                     xtype: 'textfield',
-                                    label: 'Name'
+                                    label: 'Name',
+                                    name: 'name'
                                 },
                                 {
                                     xtype: 'emailfield',
-                                    label: 'Email'
+                                    label: 'Email',
+                                    name: 'email'
                                 },
                                 {
                                     xtype: 'textareafield',
-                                    label: 'Message'
+                                    label: 'Message',
+                                    name: 'message',
+                                    height: 90
                                 }
                             ]
                         },
@@ -54,8 +119,23 @@ Ext.application({
                             xtype: 'button',
                             text: 'Send',
                             ui: 'confirm',
+
+                            // The handler is called when the button is tapped
                             handler: function() {
-                                this.up('formpanel').submit();
+
+                                // This looks up the items stack above, getting a reference to the first form it see
+                                var form = this.up('formpanel');
+
+                                // Sends an AJAX request with the form data to the url specified above (contact.php).
+                                // The success callback is called if we get a non-error response from the server
+                                form.submit({
+                                    success: function() {
+                                        // The callback function is run when the user taps the 'ok' button
+                                        Ext.Msg.alert('Thank You', 'Your message has been received', function() {
+                                            form.reset();
+                                        });
+                                    }
+                                });
                             }
                         }
                     ]
